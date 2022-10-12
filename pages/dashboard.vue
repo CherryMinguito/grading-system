d3f7f5e5a0da3b0271f739b474478903cfc75543
Lyndon Dizon <lyndon.dizon@ctu.edu.ph>
2022-10-12 12:23:31 +0800

    Update in dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+        crossorigin="anonymous">
+        <title>Welcome</title>
+        <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                <h4 class="text-white">Collapsed content</h4>
+                <span class="text-muted">Toggleable via the navbar brand.</span>
+                </div>
+            </div>
+            <nav class="navbar navbar-dark bg-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+        </button>
+                <a class="navbar-brand" href="#">DASHBOARD</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>li>
+                <div></div><div></div>
+                <router-link to="/">LOGOUT</router-link><br>
+            </nav>
+        </div>
+    </div>
+    <h4>Grading System</h4>
+    <div>
+        <h3>Grading System</h3>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+<style>
+.btn{
+    margin-left: 1100px;
+}
+</style>
