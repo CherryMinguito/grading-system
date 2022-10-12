e88a2fee8f67d2635e9b141230a9143a6410e9f2
kencosep19 <kencosep19@gmail.com>
2022-10-12 13:10:13 +0800

    Login Page added

--- components/Home.vue
+++ components/Home.vue
@@ -1,33 +1,39 @@
-<!-- Please remove this file from your project -->
+<style>
+    .container{
+        box-shadow: 5px 8px 8px #888888;
+        background-color: rgb(243, 240, 235);
+        border-radius: 10px;
+        margin-top: 15%;
+        padding: 10px;
+        height: 30vh;
+        width: 50vh;
+    }
+    form{
+        margin-left: 50px;
+    }
+    form input{
+        margin-top: 30px;
+        height: 40px;
+        width: 270px;
+    }
+    form button{
+        margin-top: 30px;
+        margin-left: 85px;
+        align-items: center;
+        width: 100px;
+    }
+</style>
 <template>
-<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
-    <form class="border shadow p-3 rouneded bg-light">
-      <h1 class="text-center p-3 text-dark">Login</h1>
-      <div class="row justify-content-center">
-              <div class="col mb-3">
-                  <label class="form-label text-dark">Username</label>
-                  <input class="form-control">
-              </div>
-      </div>
-      <div class="row justify-content-center">
-            <div class="col mb-3">
-              <label class="form-label text-dark">Password</label>
-              <input class="form-control">
-          </div>
-      </div>
-      <div class="row justify-cotent-center">
-        <div class="col mb-3">
-            <p class="text-dark">No Account yet? <a class="text-dark" href="">Sign Up</a></p>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col mb-3">
-            <b-button pill>Login</b-button>
-        </div>
-      </div>
-    </form>
-  </div>
+    <div class="container">
+        <form>
+            <input type="text" placeholder="Username"/>
+            <input type="text" placeholder="Password"/>
+            <br>
+            <button class="btn btn-primary">LOGIN</button>
+        </form>
+    </div>
 </template>
+  
 
 <script>
 export default {
