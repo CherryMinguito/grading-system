1e0d4fdf587ce83fabb0248b8380e0f0e470a431
SanchezLoinel <loineljohn.sanchez@ctu.edu.ph>
2022-10-12 15:15:40 +0800

    Updated Index with View

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
 <div>
-  <Home/>
-  <h1>Welcome Everyone!</h1>
-  <h3>Hello World!</h3>
-  <h4>Hello!</h4>
+  <div class="login_page">
+    <div class="login_form">
+  <form action="view">
+  <div class="mb-3" >
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </div>
+  </div>
 </div>
 </template>
 
@@ -13,3 +29,15 @@ export default {
   name: 'IndexPage'
 }
 </script>
+<style>
+  .login_page {
+    width: 100%;
+    height: 100%;
+  }
+
+  .login_form {
+    width: 400px;
+    margin: auto;
+    margin-top: 10%;
+  }
+</style>
