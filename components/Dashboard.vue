2210ef09713cb6c5f5131b715bf08c095d57e995
Jhon Paul B. Salazar <jhonpaulsalazar00@gmail.com>
2022-10-24 18:26:08 +0530

    Student Info

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -2,14 +2,14 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
       <Sidebar/>
-      <h1>This is Home Page</h1>
+      <h1>This is Dashboard Page</h1>
     </div>
   </template>
   
   <script>
   import Sidebar from './Sidebar';
   export default {
-    name: 'HomePage',
+    name: 'DashboardPage',
     components: {
       Sidebar,
     },
