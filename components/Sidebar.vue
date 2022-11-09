def90e32de4dd94dd4be6633e3fb649f0a8a7607
mrsunday312 <johnlloyd.cornejo@ctu.edu.ph>
2022-11-09 23:18:21 +0800

    CornejoBranch

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,34 +1,24 @@
 <template>
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Sidebar</span>
-        </a>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-            <NuxtLink to="/home">Home</NuxtLink>
-        </li>
-        <li>
-            <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-        </li>
-        <li>
-            <NuxtLink to="/lists">Students List</NuxtLink>
-        </li>
-        </ul>
-        <hr>
-        <div class="dropdown">
-        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>mdo</strong>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-        </ul>
-        </div>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="Home">Grading System</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="Home">Home</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-navbar-nav>
+                <b-nav-item href="#"><NuxtLink to="/dashboard">Dashboard</NuxtLink></b-nav-item>
+                <b-nav-item href=""><NuxtLink to="/lists">Students List</NuxtLink></b-nav-item>
+                <b-nav-item href=""><NuxtLink to="/loginpage">Logout</NuxtLink></b-nav-item>
+                </b-navbar-nav>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
     </div>
 </template>
