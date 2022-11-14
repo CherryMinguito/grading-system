cf3b60202863fd6ab7952edc8901374a641357d6
kickass21113 <loineljohn.sanchez@ctu.edu.ph>
2022-11-14 22:25:31 +0800

    Can add students in the database and delete

---
+++ components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="Home" style="margin-left: 20px; font-size:20px"><b>Grading System</b></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav" style="margin-left: 1000px">
+                    <a class="nav-item nav-link active" href="dashboard">Dashboard <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link active" href="Lists" style="margin-left: 20px">Students</a>
+                    <a class="nav-item nav-link active" href="Login" style="margin-left: 270px">Logout</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
---
+++ pages/contact_page.vue
@@ -0,0 +1,10 @@
+<template>
+    <Home/>
+  </template>
+  
+  <script>
+  export default {
+    name: 'IndexPage'
+  }
+  </script>
+  
