f1f6c92b8631dd9f22b3dca03dad1d99593f0e6c
unknown <christinecaintapan@gmail.com>
2022-11-16 10:42:03 +0800

    first test

--- components/Login.vue
+++ components/Login.vue
@@ -1,45 +1,100 @@
 <template>
-    <div>
-   <h1 class="text-center">Login Form</h1>
-   
-   <form>
-  <div class="form-group row mx-5">
-    <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
-    <div class="col-sm-10 mx-auto">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-    </div>
+  <div class="inside">
+      <form>
+          <h3>Sign In</h3>
+          <form class="container" method="POST" action="dashboard">
+          <div class="form-group">
+              <label>Email address</label>
+              <input type="email" class="form-control form-control-lg" />
+          </div>
+
+          <div class="form-group">
+              <label>Password</label>
+              <input type="password" class="form-control form-control-lg" />
+          </div>
+
+          <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+        </form>
+
+          <p class="forgot-password text-right mt-2 mb-4">
+              <router-link to="/forgot-password">Forgot password ?</router-link>
+          </p>
+      </form>
   </div>
-  <div class="form-group row mx-5">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password:</label>
-    <div class="col-sm-10 mx-auto">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-    </div>
-  </div>
-  <div class="container"> 
-    <div class="col-md-12 text-center">
-        <button type="button" class="btn-lg btn-outline-primary ">Login</button>
-    </div>
-  </div>
- 
-</form>    
-
-   </div>
-
-  
-
-
 </template>
 
-
-   
+<script>
+  export default {
+    name: 'LogInPage'
+      }
+</script>
 <style>
-.nuxt-logo {
-  height: 180px;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #e0e4f3 !important;
+  min-height: 100vh;
+  display: flex;
+  font-weight: 400;
+}
+
+body,
+html
+{
+  width: 100%;
+  height: 100%;
+}
+
+.navbar-light {
+  background-color: #ffffff;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+}
+
+.vertical-center {
+  display: flex;
+  text-align: left;
+  justify-content: center;
+  flex-direction: column;    
+}
+
+.inner-block {
+  width: 450px;
+  margin: auto;
+  background: #ffffff;
+  box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+  padding: 40px 55px 45px 55px;
+  border-radius: 15px;
+  transition: all .3s;
+}
+
+.vertical-center .form-control:focus {
+  border-color: #2554FF;
+  box-shadow: none;
+}
+
+.vertical-center h3 {
+  text-align: center;
+  margin: 0;
+  line-height: 1;
+  padding-bottom: 20px;
+}
+
+label {
+  font-weight: 500;
+}
+
+.forgot-password,
+.forgot-password a {
+  text-align: right;
+  font-size: 13px;
+  padding-top: 10px;
+  color: #7a7a7a;
+  margin: 0;
+}
+
+.forgot-password a {
+  color: #2554FF;
 }
 </style>
-
-<script>
-export default {
-  name: 'Login'
-}
-</script>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,34 +1,30 @@
 <template>
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="24"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item"> 
-           <NuxtLink to="/home">Home</NuxtLink>
-      </li>
-      <li>
-            <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-      </li>
-      <li>
-            <NuxtLink to="/students">Students</NuxtLink>
-      </li>
-    </ul>
-    <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </div>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item Nuxtlink to="/students"><strong>Christine Alissa Caintapan</strong></b-nav-item>
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                <b-nav-item Nuxtlink to="/home">Home</b-nav-item>
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                <b-nav-item Nuxtlink to="/dashboard">Dashboard</b-nav-item>
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                <b-nav-item Nuxtlink to="/students">Students</b-nav-item>
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
+                <b-nav-item Nuxtlink to="/login">Log Out</b-nav-item>
+            </b-navbar-nav>
+</b-collapse>
+ </b-navbar>
+</div>
 </template>
+<script>
+export default {
+name: 'SidebarPage'
+}
+</script>
+<style>
+ul li{
+padding: 10px;
+}
+</style>
