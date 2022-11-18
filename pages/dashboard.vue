55f201e3fd2f1f10b41627192f06b59ce4a7c93a
kensch <kendantestorcende@gmail.com>
2022-11-18 17:09:13 +0800

    New commit

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,45 @@
 <template>
-    <Dashboard/>
-  </template>
-  
-  <script>
-  export default {
-    name: 'DashboardPage'
-  }
-  </script>
+   
+  <div>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+ <a class="navbar-brand" href="#">Dashboard</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="navbarNav">
+   <ul class="navbar-nav">
+     <li class="nav-item active">
+       <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="/Students">STUDENTS</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">LOGOUT</a>
+     </li>
+   </ul>
+ </div>
+</nav>
+
+<div>
+       <h3 class="welcome">Welcome to Dashboard</h3>
+   </div>
+  </div>
+   
+</template>
+
+<script>
+   export default{
+
+   }
+</script>
+<style>
+.btn{
+   margin-left: 1100px;
+}
+.welcome{
+   margin-left: 45%;
+   margin-top: 10%;
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,55 @@
 <template>
-  <Home/>
+  <div>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+      rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+      crossorigin="anonymous">
+      <title>Login Page</title>
+      <div class="box">
+          <div class="inside">
+              <h1 class="logo">LOGIN</h1><br><br>
+              <form class="container" method="POST" action="dashboard">
+                  <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                  </div>
+                  <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                  </div><br>
+                  <button type="submit" class="btn btn-success">LOGIN</button>
+              </form>
+          </div>
+      
+  </div>
+  </div>
 </template>
 
 <script>
-import Login from '../components/Login.vue';
 export default {
-    name: "IndexPage",
-    components: { Login }
+name: 'IndexPage'
 }
 </script>
+<style>
+.box{
+  width: 100%;
+  height: 100%;
+}
+.inside{
+  width: 350px;
+  margin: auto;
+  padding-top: 100px;
+}
+.inside .logo{
+  margin-left: 30%;
+  letter-spacing: 10px;
+}
+.inside .btn{
+  margin-left: 15%;
+  width: 70%;
+}
+</style>
