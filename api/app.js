1d1428a3f6015040e5f63ae9f65573174e5be359
unknown <christinecaintapan@gmail.com>
2023-01-28 15:25:41 +0800

    secondtest

---
+++ api/app.js
@@ -0,0 +1,47 @@
+// app.js
+/*const Koa = require('koa');
+const app = new Koa();
+
+// Our First Route
+app.use(async ctx => {
+	ctx.body = 'Hello World';
+});
+
+// Bootstrap the server
+app.listen(3001); */
+
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+const cors = require('@koa/cors');
+
+app.use(cors());
+
+router.get('/getStudents', (ctx, next) =>{
+    console.log(ctx.request);
+    ctx.body = {
+        student:[{
+            id: 1,
+            firstName: 'Christine Alissa',
+            lastName: 'Caintapan',
+            address: 'Buhisan, Cebu City'
+        },{
+            id: 2,
+            firstName: 'Lyca May',
+            lastName: 'Roslinda',
+            address: 'Lapu Lapu City'
+        },{
+            id: 3,
+            firstName: 'Reiame',
+            lastName: 'Delfin',
+            address: 'Looc, Mandaue City'
+        }
+    ]}
+});
+
+app.use(router.routes())
+
+app.listen(3001);
--- nuxt.config.js
+++ nuxt.config.js
@@ -35,8 +35,12 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios'
   ],
 
+    axios:{
+
+    },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
